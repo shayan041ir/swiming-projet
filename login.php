5f788e4e2890e0bb2e653d0d7dbbe4c1139504c1
ShayanRezayi <135282886+shayan041ir@users.noreply.github.com>
2024-11-12 01:37:04 +0330

    Update login.php

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error); // اگر اتصال ناموفق بود، پیام خطا نمایش داده شود
+    die("Connection failed: " . $conn->connect_error); 
 }
 
 
