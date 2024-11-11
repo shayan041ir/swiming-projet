daaa5e4fbe888e64bc3a2feed57c936be9a99e43
ShayanRezayi <135282886+shayan041ir@users.noreply.github.com>
2024-11-12 01:32:20 +0330

    Update db.php

--- db.php
+++ db.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $dbname = "swimdb";
 
-// ایجاد اتصال
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// بررسی اتصال
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
