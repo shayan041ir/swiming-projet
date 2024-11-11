fbc3b2ef0919c3ed047c96a71d4e0fd09a778b4a
ShayanRezayi <135282886+shayan041ir@users.noreply.github.com>
2024-11-12 01:44:27 +0330

    Update update_coach.php

--- update_coach.php
+++ update_coach.php
@@ -2,12 +2,10 @@
 session_start();
 include 'db.php';
 
-// بررسی درخواست POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $old_id = $_POST['old_id'];
     $new_name = $_POST['new_name'];
 
-    // بروزرسانی نام مربی در دیتابیس
     $sql = "UPDATE coaches SET name = '$new_name' WHERE id = $old_id";
     if ($conn->query($sql) === TRUE) {
         header("Location: admin_dashboard.php");
