1e27f57faf87b26b903d7b0201072937ff07c869
ShayanRezayi <135282886+shayan041ir@users.noreply.github.com>
2024-11-12 01:26:53 +0330

    Update admin_add_admin.php

--- admin_add_admin.php
+++ admin_add_admin.php
@@ -3,15 +3,15 @@ include 'db.php';
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // دریافت اطلاعات از فرم
+
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // امن‌سازی داده‌های ورودی
+
     $username = mysqli_real_escape_string($conn, $username);
     $password = password_hash($password, PASSWORD_DEFAULT);
 
-    // اجرای دستور SQL برای افزودن ادمین جدید
+
     $sql = "INSERT INTO admins (username, password) VALUES ('$username', '$password')";
 
     if (mysqli_query($conn, $sql)) {
