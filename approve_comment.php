cd7eb5463a62d1fb685ed2564b6d367fb89c4d3d
ShayanRezayi <135282886+shayan041ir@users.noreply.github.com>
2024-11-12 01:30:08 +0330

    Update approve_comment.php

--- approve_comment.php
+++ approve_comment.php
@@ -1,23 +1,15 @@
 <?php
-// وارد کردن فایل اتصال به پایگاه داده
 include 'db.php';
-// شروع جلسه (Session)
 session_start();
 
-// بررسی اینکه آیا درخواست از نوع POST است
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // دریافت شناسه (ID) از داده‌های POST
     $id = $_POST['id'];
     
-    // ایجاد دستور SQL برای به‌روزرسانی وضعیت تایید کامنت با شناسه مشخص شده
     $sql = "UPDATE comments SET approved = 1 WHERE id = $id";
     
-    // اجرای دستور SQL و بررسی نتیجه
     if ($conn->query($sql) === TRUE) {
-        // در صورت موفقیت، کاربر به صفحه داشبورد ادمین هدایت می‌شود
         header("Location: admin_dashboard.php");
     } else {
-        // در صورت خطا، پیغام خطا نمایش داده می‌شود
         echo "Error: " . $conn->error;
     }
 }
