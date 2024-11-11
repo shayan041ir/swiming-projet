4d6d6210660ef3f05725edae33e18e197e2e30c2
ShayanRezayi <135282886+shayan041ir@users.noreply.github.com>
2024-11-12 01:27:34 +0330

    Update admin_add_user.php

--- admin_add_user.php
+++ admin_add_user.php
@@ -2,12 +2,10 @@
 include 'db.php';
 session_start();
 
-// بررسی اینکه آیا درخواست به صورت POST ارسال شده است یا خیر
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    // درج اطلاعات کاربر جدید در جدول users
     $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
 
     if ($conn->query($sql) === TRUE) {
