46f1497e48afa4ea7b1fee5947771363abeda544
ShayanRezayi <135282886+shayan041ir@users.noreply.github.com>
2024-11-12 01:27:21 +0330

    Update admin_add_ticket.php

--- admin_add_ticket.php
+++ admin_add_ticket.php
@@ -2,20 +2,19 @@
 include 'db.php';
 session_start();
 
-// بررسی اینکه آیا درخواست به صورت POST ارسال شده است یا خیر
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $count = intval($_POST['count']); // اطمینان از اینکه count یک عدد صحیح است
-    $time_slot = $_POST['user_type'] . ' - ' . $_POST['day']; // ترکیب نوع کاربر و روز برای time_slot
+    $count = intval($_POST['count']); 
+    $time_slot = $_POST['user_type'] . ' - ' . $_POST['day'];
 
-    // بررسی اینکه آیا کاربر ادمین است یا خیر
+
     $sql = "SELECT * FROM admins WHERE username='$username'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         $user_type = 'admin';
     } else {
-        // بررسی اینکه آیا کاربر یک کاربر معمولی است یا خیر
         $sql = "SELECT * FROM users WHERE username='$username'";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
@@ -23,7 +22,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-    // درج اطلاعات در جدول tickets
     $sql = "INSERT INTO tickets (username, email, count, time_slot) VALUES ('$username', '$email', $count, '$time_slot')";
     
     if ($conn->query($sql) === TRUE) {
