c81af015a1f65e49f917c1b56c227a8e725c3a1b
ShayanRezayi <135282886+shayan041ir@users.noreply.github.com>
2024-11-12 01:25:58 +0330

    Update add_ticket.php

--- add_ticket.php
+++ add_ticket.php
@@ -2,21 +2,21 @@
 include 'db.php';
 session_start();
 
-// بررسی اینکه آیا درخواست به صورت POST ارسال شده است یا خیر
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $count = intval($_POST['count']); // اطمینان از اینکه count یک عدد صحیح است
-    $time_slot = $_POST['user_type'] . ' - ' . $_POST['day']; // ترکیب نوع کاربر و روز برای time_slot
+    $count = intval($_POST['count']); 
+    $time_slot = $_POST['user_type'] . ' - ' . $_POST['day']; 
     $user_type = '';
 
-    // بررسی اینکه آیا کاربر ادمین است یا خیر
+
     $sql = "SELECT * FROM admins WHERE username='$username'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         $user_type = 'admin';
     } else {
-        // بررسی اینکه آیا کاربر یک کاربر معمولی است یا خیر
+
         $sql = "SELECT * FROM users WHERE username='$username'";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
@@ -24,13 +24,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-    // درج اطلاعات در جدول tickets
+
     $sql = "INSERT INTO tickets (username, email, count, time_slot) VALUES ('$username', '$email', $count, '$time_slot')";
 
     if ($conn->query($sql) === TRUE) {
         if ($user_type == 'admin') {
-            // header("Location: admin_dashboard.php");
-            // بررسی ارسال فرم
+
+
             if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $ticket_count = $_POST['count'];
                 $ticket_price = 15000;
@@ -38,13 +38,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $_SESSION['count'] = $ticket_count;
                 $_SESSION['ticket_price'] = $ticket_price;
 
-                // هدایت به صفحه paypal.php
+
                 header('Location: paypal.php');
                 exit();
             }
         } else {
             // header("Location: dashboard.php");
-            // بررسی ارسال فرم
+
             if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $ticket_count = $_POST['count'];
                 $ticket_price = 15000;
@@ -52,13 +52,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $_SESSION['count'] = $ticket_count;
                 $_SESSION['ticket_price'] = $ticket_price;
 
-                // هدایت به صفحه paypal.php
+
                 header('Location: paypal.php');
                 exit();
             }
         }
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error; // چاپ خطای SQL
+        echo "Error: " . $sql . "<br>" . $conn->error;
     }
 
     $conn->close();
