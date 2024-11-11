c3aeae651a3dda3e8e1e9f4010d12a1ef4389ed2
ShayanRezayi <135282886+shayan041ir@users.noreply.github.com>
2024-11-12 01:44:36 +0330

    Update update_posttext.php

--- update_posttext.php
+++ update_posttext.php
@@ -2,13 +2,11 @@
 session_start();
 include 'db.php';
 
-// بررسی درخواست POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = $_POST['id'];
     $title = $_POST['title'];
     $content = $_POST['content'];
 
-    // بروزرسانی محتوای posttext در دیتابیس
     $sql = "UPDATE posttext SET title = '$title', content = '$content' WHERE id = $id";
     if ($conn->query($sql) === TRUE) {
         echo "محتوا با موفقیت بروزرسانی شد.";
